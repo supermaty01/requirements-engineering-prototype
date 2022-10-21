e8a0cedbeae9df73e8209470d0d446e49e59e6b1
Activeone/mateo.alvarez <mateo.alvarez@activeone.co>
2022-10-21 13:13:26 -0500

    Added final pre-conceptual schema

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-// Initial variables
+const dynamicRelationshipsInfo = {
+    'PRODUCT_OWNER-DEFINES-USER_STORY': {
+        fields: ['id','title','description','role','priority','acceptance_criteria','note'],
+        messages: {
+            success: 'User story has been defined successfully!',
+            alreadyExists: 'This user story has already been defined!'
+        }
+    }
+}
 
-let userStories = [];
