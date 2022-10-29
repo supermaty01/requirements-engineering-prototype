396ae092a40fff8074a60fce271c15275a594ccc
Activeone/mateo.alvarez <mateo.alvarez@activeone.co>
2022-10-29 03:30:52 -0500

    Changed schema and refactored current code

--- constant.js
+++ constant.js
@@ -1,13 +1,13 @@
 const dynamicRelationshipsInfo = {
     'PRODUCT_OWNER-DEFINES-USER_STORY': {
-        fields: ['id','title','description','role','priority','acceptance_criteria','note'],
+        fields: ['id', 'title', 'description', 'role', 'priority', 'acceptance_criteria', 'note'],
         messages: {
             success: 'User story has been defined successfully!',
             alreadyExists: 'This user story has already been defined!'
         }
     },
     'ADMINISTRATOR-REGISTERS-SOFTWARE_SYSTEM': {
-        fields: ['name'],
+        fields: ['id', 'name'],
         messages: {
             success: 'Software System has been registered successfully!',
             alreadyExists: 'This software system has already been registered!'
@@ -56,24 +56,31 @@ const dynamicRelationshipsInfo = {
         }
     },
     'PRODUCT_OWNER-IDENTIFIES-REQUIREMENT': {
-        fields: ['stakeholder_name', 'user_story_id', 'software_system_name', 'type'],
+        fields: ['stakeholder_id', 'user_story_id', 'software_system_id', 'type'],
         messages: {
             success: 'Requirement has been identified successfully!',
             alreadyExists: 'This requirement has already been identified!'
         }
     },
     'SOFTWARE_ARCHITECT-DESIGNS-SOFTWARE_SYSTEM': {
-        fields: ['name', 'programming_language', 'framework_name', 'environment_name', 'architecture_name'],
+        fields: ['id', 'version', 'name', 'programming_language', 'framework', 'environment', 'architecture'],
         messages: {
             success: 'Software System has been designed successfully!',
             alreadyExists: 'Software System has already been designed!',
         }
     },
     'SOFTWARE_ARCHITECT-DEFINES-SOFTWARE_ELEMENT': {
-        fields: ['name', 'software_system_name', 'user_story_id', 'version'],
+        fields: ['id', 'name', 'description', 'software_system_id'],
         messages: {
             success: 'Software Element has been defined successfully!',
             alreadyExists: 'Software Element has already been defined!',
+        }
+    },
+    'DEVELOPER-DEVELOPS-SOFTWARE_ELEMENT': {
+        fields: ['id', 'interface_status', 'status', 'version'],
+        messages: {
+            success: 'Software Element has been developed successfully!',
+            alreadyExists: 'Software Element has already been developed!',
         }
     },
 }
